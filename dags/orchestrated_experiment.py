91000a9913c1120fd58b746356b63f637eb30d0a
tokimasa <edwardlai89@gmail.com>
2021-07-14 06:50:51 +0800

    add slack task

--- dags/orchestrated_experiment.py
+++ dags/orchestrated_experiment.py
@@ -4,6 +4,8 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 from airflow.utils.dates import days_ago
 from scripts.data_ingestion import ingest_data
 from scripts.data_preparing import prepare_data
@@ -33,59 +35,72 @@ dag = DAG(
 def validate_data():
 	print('show data quality...')
 
+def get_plot():
+    return "http://airbnb.io/img/projects/airflow3.png"
+    # return 'Hello, World!'
+
+
 with dag:
-	drift_detection_task = BranchPythonOperator(
+        drift_detection_task = BranchPythonOperator(
         task_id='drift_detection',
         provide_context=True,
         python_callable=drift_detection
-	)
+        )
 
-	data_ingestion_task = PythonOperator(
+        data_ingestion_task = PythonOperator(
         task_id='data_ingestion',
         python_callable=ingest_data
-	)
+        )
 
-	data_validation_task = PythonOperator(
+        data_validation_task = PythonOperator(
         task_id='data_validation',
         python_callable=validate_data
-	)
+        )
 
-	data_preparation_task = PythonOperator(
+        data_preparation_task = PythonOperator(
         task_id='data_preparation',
         python_callable=prepare_data
-	)
+        )
 
-	model_training_task = PythonOperator(
+        model_training_task = PythonOperator(
         task_id='model_training',
         provide_context=True,
         python_callable=train
-	)
+        )
 
-	model_evaluation_task = PythonOperator(
+        model_evaluation_task = PythonOperator(
         task_id='model_evaluation',
         provide_context=True,
         python_callable=evaluate
-	)
+        )
 
-	inference_task_1 = PythonOperator(
+        inference_task_1 = PythonOperator(
         task_id='inference',
         provide_context=True,
         python_callable=inference
-	)
+        )
 
-	inference_task_2 = PythonOperator(
+        inference_task_2 = PythonOperator(
         task_id='retrained_inference',
         provide_context=True,
         python_callable=inference
-	)
+        )
 
-	git_push_task = BashOperator(
+        git_push_task = BashOperator(
         task_id='git_push_task',
         # "scripts" folder is under "/usr/local/airflow/dags"
         bash_command="scripts/git_push.sh",
         trigger_rule='one_success',
         )
 
-	data_ingestion_task >> data_validation_task >> data_preparation_task >> drift_detection_task >> [model_training_task, inference_task_1]
-	model_training_task >> model_evaluation_task >> inference_task_2
-	[inference_task_1, inference_task_2] >> git_push_task
+        post_performance_task = SlackWebhookOperator(
+        task_id='post_performance',
+        http_conn_id='slack_connection',
+        webhook_token=BaseHook.get_connection('slack_connection').password,
+        message=get_plot(),
+        channel='#feed'
+        )
+
+        data_ingestion_task >> data_validation_task >> data_preparation_task >> drift_detection_task >> [model_training_task, inference_task_1]
+        model_training_task >> model_evaluation_task >> inference_task_2
+        [inference_task_1, inference_task_2] >> git_push_task >> post_performance_task
